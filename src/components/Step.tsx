3fb209319bf4905b2750145edca4d0b94622e261
unknown <aakashnaik150798@gmail.com>
2024-08-30 02:27:55 +0530

    fixed build bugs

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+
 import { Canvas } from '@react-three/fiber';
 import { Text, OrbitControls } from '@react-three/drei';
-import * as THREE from 'three';
+
 
 interface StepProps {
   position: [number, number, number];
